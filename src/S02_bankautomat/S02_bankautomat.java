6f2aa478b5843efe0eec0e8df1c9a2b31b7cf6b5
pschwarzmann <paul.schwarzmann@student.htldornbirn.at>
2023-10-13 09:34:09 +0200

    Beispiel Bankomat

--- src/S02_bankautomat/S02_bankautomat.java
+++ src/S02_bankautomat/S02_bankautomat.java
@@ -9,9 +9,9 @@ public class S02_bankautomat {
 
         char cAbbruch = 'J';
         int iEingabe = 0;
-        double dKonto = 0;
-        double dAbheben = 0;
-        double dEinzahlen = 0;
+        double dKonto = 0.0;
+        double dAbheben = 0.0;
+        double dEinzahlen = 0.0;
         Scanner scanner = new Scanner(System.in);
 
         do{
@@ -19,27 +19,24 @@ public class S02_bankautomat {
             System.out.print("Menü:\nDrücke 1 um Einzuzahlen!\nDrücke 2 um Abzuheben!\nDrücke 3 um den Kontostand zu sehen!\nDrücke 4 um die Eingabe zu beenden!\n");
             iEingabe = scanner.nextInt();
 
-            System.out.println();
 
             if(iEingabe == 1)
             {
                 System.out.println("Geben Sie den Betrag ein den Sie einzahlen wollen!:");
-                dEinzahlen = scanner.nextInt();
+                dEinzahlen = scanner.nextDouble();
 
                 dKonto = dKonto + dEinzahlen;
 
-                System.out.println();
             }
 
 
             if(iEingabe == 2)
             {
                 System.out.println("Geben Sie den Betrag ein den Sie abheben wollen!:");
-                dAbheben = scanner.nextInt();
+                dAbheben = scanner.nextDouble();
 
                 dKonto = dKonto - dAbheben;
 
-                System.out.println();
             }
 
 
@@ -55,11 +52,10 @@ public class S02_bankautomat {
                 System.out.println("Möchten Sie das Programm beenden? (Wenn Ja tippen Sie: J ansonst N)");
                 cAbbruch = scanner.next().charAt(0);
 
-                System.out.println();
             }
 
-
-
+            System.out.println("Möchten Sie das Programm beenden? (Wenn Ja tippen Sie: J ansonst N)");
+            cAbbruch = scanner.next().charAt(0);
 
         }while(cAbbruch != 'J');
         System.out.print("Beendet");
