1827194a06943f5b010baff9fe6eb8182234df97
pschwarzmann <paul.schwarzmann@student.htldornbirn.at>
2023-10-18 08:52:50 +0200

    StringHelper auf Lock

--- src/S05_StringHelper/StringHelper.java
+++ src/S05_StringHelper/StringHelper.java
@@ -1,4 +1,23 @@
 package S05_StringHelper;
 
 public class StringHelper {
+    public static int countLetters(String inputString, char targetChar) {
+        int count = 0;
+
+        for (int i = 0; i < inputString.length(); i++) {
+            if (inputString.charAt(i) == targetChar) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String input = "Hallo Welt!";
+        char target = 'l';
+        int ergebnis = countLetters(input, target);
+
+        System.out.println("Der Char '" + target + "' kommt " + ergebnis + " mal im text vor!");
+    }
 }
