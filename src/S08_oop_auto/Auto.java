287232804547efb77d4fbd04137f7b424456673b
pschwarzmann <paul.schwarzmann@student.htldornbirn.at>
2023-11-10 09:35:43 +0100

    update auf lock

---
+++ src/S08_oop_auto/Auto.java
@@ -0,0 +1,73 @@
+package S08_oop_auto;
+
+public class Auto {
+
+    public int setiFuel;
+    private int iLeistung = 0;
+
+    private int iFuel = 0;
+
+    private String sBrand = "";
+
+    private String sSerialnumber = "";
+
+    private int iFualkap = 300;
+
+    private String sHonk = "";
+
+
+
+    //get und setter
+
+
+    public String getsHonk() {
+        return sHonk;
+    }
+
+    public void setsHonk(String sHonk) {
+        this.sHonk = sHonk;
+    }
+
+    public int getiFualkap() {
+        return iFualkap;
+    }
+
+    public void setiFualkap(int iFualkap) {
+        this.iFualkap = iFualkap;
+    }
+
+    public int getiLeistung() {
+        return iLeistung;
+    }
+
+    public void setiLeistung(int iLeistung) {
+        this.iLeistung = iLeistung;
+    }
+
+    public int getiFuel() {
+        return iFuel;
+    }
+
+    public void setiFuel() {
+        this.iFuel = iFuel;
+    }
+
+    public String getsBrand() {
+        return sBrand;
+    }
+
+    public void setsBrand(String sBrand) {
+        this.sBrand = sBrand;
+    }
+
+    public String getsSerialnumber() {
+        return sSerialnumber;
+    }
+
+    public void setsSerialnumber(String sSerialnumber) {
+        this.sSerialnumber = sSerialnumber;
+    }
+
+    public void setiFuel(int i) {
+    }
+}
---
+++ src/S08_oop_auto/TestIT.java
@@ -0,0 +1,54 @@
+package S08_oop_auto;
+
+import java.util.Scanner;
+
+public class TestIT {
+
+    public static void main(String[] args) {
+
+        //Deklaration
+        double dFaktor = 0.10;
+        Scanner scanner = new Scanner(System.in);
+        int iEingabe = 0;
+        int iAmountofRepetitions = 0;
+
+        //Ausgabe Auto
+        Auto auto = new Auto();
+        auto.setiLeistung(200);
+        auto.setiFuel();
+        auto.setsBrand("Porsche");
+        auto.setsSerialnumber("AC1234");
+        auto.setiFualkap(300);
+        auto.setsHonk("");
+
+        //Menü
+        System.out.println("--------------------------Menue--------------------------");
+        System.out.println("Was moechten Sie machen?");
+        System.out.println("1 - Auto\n2 - Turboboost \n3 - Hupen");
+        System.out.print("->");
+        iEingabe = scanner.nextInt();
+
+
+        // Turboboost
+        if (iEingabe == 2){
+            System.out.println("Wie viel Tank ist im Auto(bis 300 Liter möglich))");
+            auto.setiFuel = scanner.nextInt();
+            if (auto.getiFuel() > auto.getiFualkap()*dFaktor){
+                System.out.println("SuperBoostMode!");
+            }
+            else {
+                System.out.println("Not enough fuel!");
+            }
+        }
+
+        //Hupen
+        if (iEingabe == 3){
+            System.out.println("Wie viel mal möchten Sie Hupen?");
+            iAmountofRepetitions = scanner.nextInt();
+            for (int i = 0; i < iAmountofRepetitions; i++){
+                System.out.println("Tuut");
+            }
+        }
+
+    }
+}
